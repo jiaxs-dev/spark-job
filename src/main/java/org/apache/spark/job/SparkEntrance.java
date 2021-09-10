c486f97e500f3128fe627220821a2b6ee1d520b7
jiaxun19920218 <2572104454@qq.com>
2021-09-10 15:52:27 +0800

    spark job

---
+++ src/main/java/org/apache/spark/job/SparkEntrance.java
@@ -0,0 +1,10 @@
+package org.apache.spark.job;
+
+/**
+ * @description spark入口
+ * @author jiaxiansun
+ * @createTime 2021/9/10 15:08
+ * @version 1.0
+ */
+public class SparkEntrance {
+}
---
+++ src/test/java/org/apache/spark/job/AppTest.java
@@ -0,0 +1,20 @@
+package org.apache.spark.job;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
